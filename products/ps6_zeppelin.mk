b7978b8c2b7dd89b8c36893efe2088201695b590
Eashwar Ranganathan <crystal.dome@gmail.com>
2011-07-05 01:34:00 -0700

    Revert d190ee0c686087c54af3e5963df257c4d16ff62e^..HEAD

--- products/ps6_zeppelin.mk
+++ products/ps6_zeppelin.mk
@@ -21,13 +21,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Extra Zeppelin overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/phoenixsong6/overlay/zeppelin
-<<<<<<< HEAD
-
-# Post Shell Commands
-ifeq ($(HOST_OS), darwin)
-$(shell sed -ie '19s/ro.product.device=zeppelin/ro.product.device=zepp/' out/target/product/zeppelin/system/build.prop)
-else
-$(shell sed -i '19s/ro.product.device=zeppelin/ro.product.device=zepp/' out/target/product/zeppelin/system/build.prop)
-endif
-=======
->>>>>>> parent of d190ee0... experimental: Shell Command
